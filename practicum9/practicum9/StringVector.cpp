7c29ce1bc23a2266c5329b4db24ae5904444933f
Daniel Stefanov <97540472+ludsmotika@users.noreply.github.com>
2024-04-19 19:56:21 +0300

    practicum9

---
+++ practicum9/practicum9/StringVector.cpp
@@ -0,0 +1,200 @@
+#include "StringVector.h"
+
+StringVector::StringVector()
+{
+	strings = new MyString[capacity];
+}
+
+StringVector::StringVector(const StringVector& other)
+{
+	copyFrom(other);
+}
+
+StringVector::StringVector(StringVector&& other) noexcept
+{
+	moveFrom(std::move(other));
+}
+
+void StringVector::moveFrom(StringVector&& other) 
+{
+	this->strings = other.strings;
+	other.strings = nullptr;
+
+	this->capacity = other.capacity;
+	other.capacity = 0;
+
+	this->stringsCount = other.stringsCount;
+	other.stringsCount = 0;
+}
+
+StringVector& StringVector::operator= (StringVector&& other)
+{
+	if (this != &other)
+	{
+		free();
+		moveFrom(std::move(other));
+	}
+
+	return *this;
+}
+
+const StringVector& StringVector::operator= (const StringVector& other)
+{
+	if (this != &other)
+	{
+		free();
+		copyFrom(other);
+	}
+	return *this;
+}
+
+StringVector::~StringVector()
+{
+	free();
+}
+
+void StringVector::free()
+{
+	delete[] strings;
+}
+
+void StringVector::pop_back()
+{
+	if (!size())
+		throw "Empty vector";
+
+	erase(size() - 1);
+}
+
+void StringVector::push_back(const MyString& string)
+{
+	insert(size(), string);
+}
+
+void StringVector::push_back(MyString&& string)
+{
+	insert(size(), std::move(string));
+}
+
+void StringVector::insert(unsigned index, MyString&& string)
+{
+	if (index<0 || index > size())
+		throw "Invalid index!";
+
+	stringsCount++;
+
+	if (stringsCount >= capacity)
+	{
+		resize();
+	}
+
+	for (size_t i = size() - 1; i > index; i--)
+	{
+		strings[i] = std::move(strings[i - 1]);
+	}
+
+	strings[index] = std::move(string);
+}
+
+void StringVector::insert(unsigned index, const MyString& string)
+{
+	if (index<0 || index > size())
+		throw "Invalid index!";
+
+	stringsCount++;
+
+	if (stringsCount >= capacity)
+	{
+		resize();
+	}
+
+	for (size_t i = size() - 1; i > index; i--)
+	{
+		strings[i] = strings[i - 1];
+	}
+
+	strings[index] = string;
+}
+
+
+void StringVector::copyFrom(const StringVector& other)
+{
+	strings = new MyString[other.getCapacity()];
+
+	for (size_t i = 0; i < other.size(); i++)
+	{
+		strings[i] = other.strings[i];
+	}
+
+	stringsCount = other.size();
+	capacity = other.getCapacity();
+}
+
+void StringVector::clear()
+{
+	free();
+	stringsCount = 0;
+	strings = new MyString[capacity];
+}
+
+const MyString& StringVector::operator[] (int index) const
+{
+	return strings[index];
+}
+
+MyString& StringVector::operator[] (int index)
+{
+	return strings[index];
+}
+
+size_t StringVector::getCapacity() const
+{
+	return capacity;
+}
+
+unsigned StringVector::erase(unsigned index)
+{
+	if (index < 0 || index >= size())
+		throw "Invalid index!";
+
+
+	for (size_t i = index; i < size(); i++)
+	{
+		strings[i] = strings[i + 1];
+	}
+
+	stringsCount--;
+
+	return index;
+}
+
+bool StringVector::empty() const
+{
+	if (!stringsCount)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
+size_t StringVector::size() const
+{
+	return stringsCount;
+}
+
+void StringVector::resize()
+{
+	capacity *= 2;
+	MyString* newStrings = new MyString[capacity];
+
+	for (size_t i = 0; i < size(); i++)
+	{
+		newStrings[i] = strings[i];
+	}
+
+	delete[] strings;
+
+	strings = newStrings;
+}
---
+++ practicum9/practicum9/StringVector.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "MyString.h";
+
+class StringVector
+{
+public:
+	StringVector(); //
+	StringVector(const StringVector& other);//
+	const StringVector& operator= (const StringVector& other);//
+	StringVector(StringVector&& other) noexcept; //
+	StringVector& operator= (StringVector&& other);//
+
+	~StringVector();//
+
+	size_t size() const;//
+	bool empty() const;//
+	unsigned erase(unsigned index);//
+	void clear();//
+	size_t getCapacity() const;//
+
+	void insert(unsigned index, const MyString& string);//
+	void insert(unsigned index, MyString&& string);//
+
+	void push_back(const MyString& string);//
+	void push_back(MyString&& string);//
+	void pop_back();//
+
+	const MyString& operator[] (int index) const;
+	MyString& operator[] (int index);
+
+private:
+	void free();//
+	void copyFrom(const StringVector& other);//
+	void moveFrom(StringVector&& other);
+	void resize();//
+	MyString* strings = nullptr;
+	size_t stringsCount = 0;
+	size_t capacity = 4;
+};
